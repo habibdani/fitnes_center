1d2af545df7afbb0275c563cdaa0179a28541380
habibdani <habibdani86@gmail.com>
2023-12-09 11:00:12 +0700

    frist commit

---
+++ src/main/java/fitnescenter/endpoit/FitnerCenterEndpointApplication.java
@@ -0,0 +1,13 @@
+package fitnescenter.endpoit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnerCenterEndpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnerCenterEndpointApplication.class, args);
+	}
+
+}
