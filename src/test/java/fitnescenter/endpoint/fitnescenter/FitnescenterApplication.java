840f3e4f0cc85fe0b3bc969b221327739d072abf
habibdani <habibdani86@gmail.com>
2023-12-18 10:25:33 +0700

    update testapp

---
+++ src/test/java/fitnescenter/endpoint/fitnescenter/FitnescenterApplication.java
@@ -0,0 +1,15 @@
+package fitnescenter.endpoint.fitnescenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnescenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnescenterApplication.class, args);
+	}
+
+}
+
